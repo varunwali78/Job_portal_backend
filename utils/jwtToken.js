81911b2ed5bd6466ce6b2990727b0481107c7262
varunwali <varunwali36@gmail.com>
2024-03-11 23:49:40 +0530

    commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,7 +4,9 @@ export const sendToken = (user, statusCode, res, message) => {
     expiresIn: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 //1d
     ),
-    httpOnly: true, // Set httpOnly to true
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
