fa1e76d8df0a3b0af263dbf3db8e8ef6882c0c72
varunwali <varunwali36@gmail.com>
2024-03-11 15:42:32 +0530

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { login, register, logout, getUser } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  logout,
+  getUser,
+} from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", isAuthenticated, logout);
-router.get("/getuser", isAuthenticated, getUser);
+router.get("/logout", logout);
+router.get("/getuser", getUser);
 
 export default router;
