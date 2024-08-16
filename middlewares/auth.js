e4a305f8d47c8bb494b6a232d7ba53b2bbf2cad4
varunwali <varunwali36@gmail.com>
2024-08-16 21:44:23 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.headers;
+  const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
