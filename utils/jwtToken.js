7057b181fc39c52f4a11eb62bb45f166d184d9ea
varunwali <varunwali36@gmail.com>
2024-08-17 13:31:13 +0530

    commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,8 +1,6 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
 
-  const isProduction = process.env.NODE_ENV === "production";
-
   res
     .status(statusCode)
     .cookie("token", token, {
@@ -10,8 +8,8 @@ export const sendToken = (user, statusCode, res, message) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // 1d
       ),
       httpOnly: true,
-      secure: isProduction, // Required for SameSite=None
-      sameSite: "None", // Must be None for cross-site cookies
+      secure: true, // Force secure cookies
+      sameSite: "None", // Force cross-site cookies
     })
     .json({
       success: true,
