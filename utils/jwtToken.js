11eedae5f8ef7cc5051703c990ba81a8bf1584fb
varunwali <varunwali36@gmail.com>
2024-08-17 12:59:45 +0530

    commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,18 +1,17 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
 
-  // Determine if we are in a production environment
   const isProduction = process.env.NODE_ENV === "production";
 
   res
     .status(statusCode)
     .cookie("token", token, {
       expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 //1d
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // 1d
       ),
-      httpOnly: true, // Ensures the cookie is only accessible by the web server
-      secure: isProduction, // Ensure the cookie is sent over HTTPS in production
-      sameSite: isProduction ? "None" : "Lax", // Adjust SameSite attribute depending on the environment
+      httpOnly: true,
+      secure: isProduction, // Required for SameSite=None
+      sameSite: "None", // Must be None for cross-site cookies
     })
     .json({
       success: true,
